aef568130123422ca016f5e39dc644823ffbb4c7
provos <provos@5521b004-cb4d-0410-95fd-d1d2b393e6e4>
2008-04-23 19:01:34 +0000

    make collect work

--- wrap.h
+++ wrap.h
@@ -1,7 +1,7 @@
 #ifndef _WRAP_H_
 #define _WRAP_H_
 
-PyObject *wrap_ncap_msg_to_python(ncap_msg_t msg);
+PyObject *wrap_ncap_msg_to_python(ncap_msg_ct msg);
 ncap_msg_t wrap_python_to_ncap_msg(PyObject *);
 
 #endif /* _WRAP_H_ */
